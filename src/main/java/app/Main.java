d2bddf8572aaa3eb64aac600c965faaf43db32c1
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-03-20 22:23:16 -0400

    Add endpoint

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,18 @@
 package app;
 import io.javalin.Javalin;
+import service.AuthService;
+import service.InputRequest;
 import service.TestService;
-
+import com.fasterxml.jackson.databind.*;
 public class Main {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
+        var app = Javalin.create(config -> {
+                                     config.bundledPlugins.enableCors(cors -> {
+                                         cors.addRule(it -> {
+                                             it.anyHost();
+                                         });
+                                     });
+                                 })
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
 
@@ -12,6 +20,15 @@ public class Main {
             var testService = new TestService();
             ctx.result("" + testService.countLocations());
 
+        });
+
+        app.post("/admin/saveData", ctx->{
+            var authService = new AuthService();
+
+            var data = ctx.bodyAsClass(InputRequest.class);
+            authService.validate(data);
+            System.out.println(data);
+            ctx.result("Test");
         });
     }
 }
