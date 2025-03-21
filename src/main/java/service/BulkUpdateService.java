d8d274eecad1cd5ac158d8e49785b5d066246a1f
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-03-21 17:16:50 -0400

    Updates

--- src/main/java/service/BulkUpdateService.java
+++ src/main/java/service/BulkUpdateService.java
@@ -1,12 +1,18 @@
 package service;
 
 import app.data.Data;
+import app.data.Group;
+import database.GroupsRepository;
 
 public class BulkUpdateService {
-    public void bulkUpdate(Data data) {
+    public void bulkUpdate(Data data) throws Exception{
         System.out.println(data.getGroups());
         System.out.println(data.getConventions());
         System.out.println(data.getGameStores());
         System.out.println(data.getGameRestaurants());
+
+
+        GroupsRepository groupsRepository = new GroupsRepository();
+        groupsRepository.insertGroups(data.getGroups());
     }
 }
