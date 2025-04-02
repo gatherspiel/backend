4ef6194d4edc9bf4f1f35b98aefc29780ad2fb22
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-04-02 18:23:20 -0400

    Update

---
+++ src/main/java/database/search/SearchRepository.java
@@ -0,0 +1,12 @@
+package database.search;
+
+import app.result.GroupSearchResult;
+
+import java.time.DayOfWeek;
+import java.util.HashMap;
+
+public class SearchRepository {
+    public GroupSearchResult getGroups(SearchParams searchParams){
+
+    }
+}
