7ee2346332fd6019ce88c697a5203fa360a5e25a
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-04-02 20:49:10 -0400

    Updates

--- src/main/java/app/result/GroupSearchResult.java
+++ src/main/java/app/result/GroupSearchResult.java
@@ -1,4 +1,58 @@
 package app.result;
 
+import app.data.Event;
+import app.data.Group;
+import org.apache.logging.log4j.Logger;
+import utils.LogUtils;
+
+import java.time.DayOfWeek;
+import java.util.HashMap;
+
 public class GroupSearchResult {
+
+    private HashMap<Integer, Group> groupData;
+
+    Logger logger;
+
+    public GroupSearchResult(){
+        groupData = new HashMap<Integer,Group>();
+        logger = LogUtils.getLogger();
+    }
+
+    public void addGroup(Integer id, String name, String url, String summary){
+        if(!groupData.containsKey(id)) {
+            Group group = new Group();
+            group.setId(id);
+            group.setName(name);
+            group.setUrl(url);
+            group.setSummary(summary);
+            groupData.put(id, group);
+        }
+    }
+
+    public void addEvent(
+            Integer groupId,
+            Integer eventId,
+            String name,
+            String description,
+            DayOfWeek dayOfWeek,
+            String address){
+        if(!groupData.containsKey(groupId)) {
+            logger.warn("Group with id {} does not exist. Event will not be added to group search result", groupId);
+            return;
+        }
+
+        Group group = groupData.get(groupId);
+
+        Event event = new Event();
+        event.setName(name);
+        event.setSummary(description);
+        event.setDay(dayOfWeek.name());
+        event.setLocation(address);
+        event.setId(eventId);
+        group.addEvent(event);
+
+
+    }
+
 }
--- src/main/java/database/search/SearchRepository.java
+++ src/main/java/database/search/SearchRepository.java
@@ -2,11 +2,54 @@ package database.search;
 
 import app.result.GroupSearchResult;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.DayOfWeek;
-import java.util.HashMap;
 
 public class SearchRepository {
-    public GroupSearchResult getGroups(SearchParams searchParams){
+    public GroupSearchResult getGroups(GroupSearchParams searchParams, Connection conn) throws Exception{
 
+        PreparedStatement statement = searchParams.getSearchQuery(conn);
+        ResultSet rs = statement.executeQuery();
+
+        GroupSearchResult searchResult = new GroupSearchResult();
+        while(rs.next()){
+            Integer groupId = rs.getInt("id");
+
+            String groupName = rs.getString("name");
+            String url = rs.getString("url");
+            String groupSummary = rs.getString("summary");
+            searchResult.addGroup(groupId, groupName, url, groupSummary);
+
+            Integer eventId = rs.getInt("eventId");
+            String eventName = rs.getString("eventname");
+            String description = rs.getString("description");
+            DayOfWeek dayOfWeek = DayOfWeek.valueOf(rs.getString("day_of_week"));
+
+            String streetAddress = rs.getString("street_address");
+            String city = rs.getString("city");
+            String state = rs.getString("state");
+            String zipCode = rs.getString("zip_code");
+
+            String address = streetAddress + ", " + city + ", " + state + " " + zipCode;
+            searchResult.addEvent(
+                    groupId,
+                    eventId,
+                    eventName,
+                    description,
+                    dayOfWeek,
+                    address
+            );
+        }
+        return searchResult;
+
+        //TODO
+        /*
+            Run unit tests to make sure default test case works.
+            Make sure search works with filters.
+            Create API endpoint.
+
+         */
     }
 }
